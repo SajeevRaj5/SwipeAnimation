a2964b51e9be6f5e80dca96b8d81100876f41c99
Sajeev Raj <sajeevraj.1989@gmail.com>
2019-04-02 20:22:12 +0400

    Initial Commit

---
+++ SwipeAnimationSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwipeAnimationSample
+//
+//  Created by sajeev Raj on 4/2/19.
+//  Copyright © 2019 Sajeev Arya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
